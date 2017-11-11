f1376b37ebaca4e0963d2839d13ede2fb0b05925
tuonglinh <nguyenvantuonglinh@gmail.com>
2017-11-11 08:59:54 +0700

    fix

--- resources/views/account/list-billdetail.blade.php
+++ resources/views/account/list-billdetail.blade.php
@@ -13,29 +13,6 @@
   <div class="main">
     <div class="container">
         <div class="row">
-            <div class="col-sm-3 col-left">
-                <img src="images/lienhe.gif" alt="lien he qc" style="max-width:100%; margin-top: 20px; min-width:100%">
-                <form class="form-search-nxp" method="GET" action="search-is-price">
-                    <div class="form-nxp">
-                        <h3>Search</h3>
-                    </div>
-                    <label class="nxp-111"><b>from</b></label>
-                    <input type="text" name="min" value="" placeholder="100$"><br>
-                    <label class="nxp-111"><b>To</b></label>
-                    <input type="text" name="max" value="" placeholder="1000$"><br>
-                    <div class="center">
-                        <button class="btn-danger" type="submit" name="button">Search</button>
-                    </div>
-                </form><br>
-                <div class="banner-left"><a href="#"><img src="images/products/0.png" alt="hinh anh"></a>
-                    <div class="banner-content">
-                        <h1>Khuyến mãi</h1>
-                        <h2>20%</h2>
-                        <p></p>
-                        <a href="#">mua ngay</a>
-                    </div>
-                </div>
-            </div>
             <div class="col-sm-9 col-right">
                 <h2>ĐƠN HÀNG #{{ $id }}</h2>
 
